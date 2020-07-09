c42f564188095765e0217996518d4b467e803167
gtn223@nyu.edu <gtn223@nyu.edu>
2020-07-09 15:18:33 -0400

    edits

--- PythonScripts/runme.bat
+++ PythonScripts/runme.bat
@@ -3,4 +3,4 @@
 FOR /F "tokens=*" %%a in ('dir /b *.ma') do SET MAYA=%%a
 FOR /F "tokens=*" %%a in ('dir /b *.csv') do SET CSV=%%a
 
-cmd /k "pip install virtualenv & virtualenv venv & cd /d venv\Scripts & activate & cd /d  ../.. & pip install -r requirements.txt & python MayaFuse.py %MAYA% %CSV% runserver & deactivate"
+cmd /k "pip install virtualenv & virtualenv venv & cd /d venv\Scripts & activate & cd /d  ../.. & pip install -r requirements.txt & python Rename.py %MAYA% %CSV% runserver & deactivate"
